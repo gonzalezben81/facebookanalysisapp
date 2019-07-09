44fc7797b2aada3d36707767f954aec6a4526c07
gcdqw <ben.gonzalez@monsanto.com>
2019-07-09 14:08:52 -0500

    Added heartbeat chart to analysis app.

--- FacebookAnalyzer/app.R
+++ FacebookAnalyzer/app.R
@@ -32,7 +32,8 @@ ui <- fluidPage(
          tabPanel(title = 'Emotional Percentages Table:',DT::DTOutput("json_text")),
          tabPanel(title = 'Sentence Table:',DT::DTOutput("sentence_table")),
          tabPanel(title = 'Emotions Bar Plot:',plotOutput("plot_one")),
-         tabPanel(title = 'Pos vs Neg Bar Plot:',plotOutput("plot_two"))
+         tabPanel(title = 'Pos vs Neg Bar Plot:',plotOutput("plot_two")),
+         tabPanel(title = 'Heartbeat Plot:',plotOutput("heartbeat"))
         )
       )
    )
@@ -81,6 +82,7 @@ server <- function(input, output) {
   })
   
   
+  ###Sentiment reactive function
   stuff<- eventReactive(input$go,{
     
     text<- text_content()
@@ -104,20 +106,11 @@ server <- function(input, output) {
     
     value<- stuff()
     
-    # prop.table(value[,1:8])
-    # ##Create the sentiment scores table
-    # sentimentscores <- round(colSums(prop.table((value[,1:8])))*100,digits = 1)
-    # # print(sentimentscores)
-    # ##Create a dataframe that contains the sentiment scores
-    # sentimentscores <- as.data.frame(sentimentscores)
-    # ##Rename the column names of the sentiment scores
-    # colnames(sentimentscores) <- c("Percentages")
-    
     DT::datatable(value)
   })
   
   
-  
+ ###Emotions Output
  output$plot_one <- renderPlot({
    
    text<- text_content()
@@ -136,6 +129,7 @@ server <- function(input, output) {
    barplot_one
  })
  
+ ##Positive and Negative Emotions Output
  output$plot_two <- renderPlot({
    
    text<- text_content()
@@ -154,7 +148,7 @@ server <- function(input, output) {
    barplot_two
  })
  
- 
+ ###Sentence Table Output
  output$sentence_table <- DT::renderDataTable({
    
    text<- text_content_send()
@@ -162,6 +156,26 @@ server <- function(input, output) {
    values <- as.data.frame(text)
    
    DT::datatable(values)
+   
+ })
+ 
+ output$heartbeat <- renderPlot({
+   
+   docs<- text_content()
+   s_v <- get_sentences(docs)
+   s_v_sentiment <- get_sentiment(s_v)
+   # name<- gsub(pattern = './messages',replacement = "",x = file_name)
+   # name <- gsub(pattern = ".txt*",replacement = "",x = name)
+   # name <- gsub(pattern = "/",replacement = "",x = name)
+   # myfile_path<- file.path(".","nrc_heartbeat",paste0(name,"timeline heartbeat.pdf"))
+   # pdf(file = myfile_path)
+   plot(
+     s_v_sentiment,
+     type="l",
+     main= " Messenger Timeline",
+     xlab = "Messenger Timeline",
+     ylab= "Emotional Valence"
+   )
    
  })
   
